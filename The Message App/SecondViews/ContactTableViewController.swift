df58f2cb4e4aef34033eee40b6b7f0123e30139b
Vibol's Macbook Pro <vibolsmacbookpro@vibolss-macbook-pro.local>
2019-01-16 23:04:48 +0700

    - design contact View - contact view coding

---
+++ The Message App/SecondViews/ContactTableViewController.swift
@@ -0,0 +1,318 @@
+//
+//  ContactTableViewController.swift
+//  The Message App
+//
+//  Created by Vibol's Macbook Pro on 1/15/19.
+//  Copyright © 2019 Vibol Roeun. All rights reserved.
+//
+
+import UIKit
+import Contacts
+import FirebaseStorage
+import ProgressHUD
+
+class ContactTableViewController: UITableViewController, UISearchResultsUpdating, UsersTableViewCellDelegate {
+
+    
+    var users: [FUser] = []
+    var matchedUsers: [FUser] = []
+    var filteredMatchedUsers: [FUser] = []
+    var allUsersGrouped = NSDictionary() as! [String : [FUser]]
+    var sectionTitleList: [String] = []
+    
+    var isGroup = false
+    var memberIdsOfGroupChat: [String] = []
+    var membersOfGroupChat: [FUser] = []
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    lazy var contacts: [CNContact] = {
+        
+        let contactStore = CNContactStore()
+        
+        let keysToFetch = [
+            CNContactFormatter.descriptorForRequiredKeys(for: .fullName),
+            CNContactEmailAddressesKey,
+            CNContactPhoneNumbersKey,
+            CNContactImageDataAvailableKey,
+            CNContactThumbnailImageDataKey] as [Any]
+        
+        // Get all the containers
+        var allContainers: [CNContainer] = []
+        
+        do {
+            allContainers = try contactStore.containers(matching: nil)
+        } catch {
+            print("Error fetching containers")
+        }
+        
+        var results: [CNContact] = []
+        
+        // Iterate all containers and append their contacts to our results array
+        for container in allContainers {
+            
+            let fetchPredicate = CNContact.predicateForContactsInContainer(withIdentifier: container.identifier)
+            
+            do {
+                let containerResults = try     contactStore.unifiedContacts(matching: fetchPredicate, keysToFetch: keysToFetch as! [CNKeyDescriptor])
+                results.append(contentsOf: containerResults)
+            } catch {
+                print("Error fetching results for container")
+            }
+        }
+        
+        return results
+    }()
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        //to remove empty cell lines
+        tableView.tableFooterView = UIView()
+        
+        //        loadUsers()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Contacts"
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.searchController = searchController
+        
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        
+        //        setupButtons()
+    }
+    
+    //MARK: TableViewDataSource
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        if searchController.isActive && searchController.searchBar.text != "" {
+            return 1
+        } else {
+            return self.allUsersGrouped.count
+        }
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if searchController.isActive && searchController.searchBar.text != "" {
+            return filteredMatchedUsers.count
+        } else {
+            // find section title
+            let sectionTitle = self.sectionTitleList[section]
+            
+            // find users for given section title
+            let users = self.allUsersGrouped[sectionTitle]
+            
+            // return count for users
+            return users!.count
+        }
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! UsersTableViewCell
+        
+        var user: FUser
+        
+        if searchController.isActive && searchController.searchBar.text != "" {
+            user = filteredMatchedUsers[indexPath.row]
+        } else {
+            
+            let sectionTitle = self.sectionTitleList[indexPath.section]
+            //get all users of the section
+            let users = self.allUsersGrouped[sectionTitle]
+            
+            user = users![indexPath.row]
+        }
+        
+        cell.delegate = self
+        cell.generateCellWith(fUser: user, indexPath: indexPath)
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        if searchController.isActive && searchController.searchBar.text != "" {
+            return ""
+        } else {
+            return self.sectionTitleList[section]
+        }
+    }
+    
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        if searchController.isActive && searchController.searchBar.text != "" {
+            return nil
+        } else {
+            return self.sectionTitleList
+        }
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+        return index
+    }
+    
+    
+    //MARK: TableViewDelegate
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+    }
+    
+    
+    
+    func compareUsers() {
+        
+        for user in users {
+            
+            if user.phoneNumber != "" {
+                
+                let contact = searchForContactUsingPhoneNumber(phoneNumber: user.phoneNumber)
+                
+                //if we have a match, we add to our array to display them
+                if contact.count > 0 {
+                    matchedUsers.append(user)
+                }
+                
+                self.tableView.reloadData()
+                
+            }
+        }
+        //        updateInformationLabel()
+        
+        self.splitDataInToSection()
+    }
+    
+    //MARK: Contacts
+    
+    func searchForContactUsingPhoneNumber(phoneNumber: String) -> [CNContact] {
+        
+        var result: [CNContact] = []
+        
+        //go through all contacts
+        for contact in self.contacts {
+            
+            if !contact.phoneNumbers.isEmpty {
+                
+                //get the digits only of the phone number and replace + with 00
+                let phoneNumberToCompareAgainst = updatePhoneNumber(phoneNumber: phoneNumber, replacePlusSign: true)
+                
+                //go through every number of each contac
+                for phoneNumber in contact.phoneNumbers {
+                    
+                    let fulMobNumVar  = phoneNumber.value
+                    let countryCode = fulMobNumVar.value(forKey: "countryCode") as? String
+                    let phoneNumber = fulMobNumVar.value(forKey: "digits") as? String
+                    
+                    let contactNumber = removeCountryCode(countryCodeLetters: countryCode!, fullPhoneNumber: phoneNumber!)
+                    
+                    //compare phoneNumber of contact with given user's phone number
+                    if contactNumber == phoneNumberToCompareAgainst {
+                        result.append(contact)
+                    }
+                    
+                }
+            }
+        }
+        
+        return result
+    }
+    
+    
+    func updatePhoneNumber(phoneNumber: String, replacePlusSign: Bool) -> String {
+        
+        if replacePlusSign {
+            return phoneNumber.replacingOccurrences(of: "+", with: "").components(separatedBy: NSCharacterSet.decimalDigits.inverted).joined(separator: "")
+            
+        } else {
+            return phoneNumber.components(separatedBy: NSCharacterSet.decimalDigits.inverted).joined(separator: "")
+        }
+    }
+    
+    
+    func removeCountryCode(countryCodeLetters: String, fullPhoneNumber: String) -> String {
+        
+        let countryCode = CountryCode()
+        
+        let countryCodeToRemove = countryCode.codeDictionaryShort[countryCodeLetters.uppercased()]
+        
+        //remove + from country code
+        let updatedCode = updatePhoneNumber(phoneNumber: countryCodeToRemove!, replacePlusSign: true)
+        
+        //remove countryCode
+        let replacedNUmber = fullPhoneNumber.replacingOccurrences(of: updatedCode, with: "").components(separatedBy: NSCharacterSet.decimalDigits.inverted).joined(separator: "")
+        
+        
+        //        print("Code \(countryCodeLetters)")
+        //        print("full number \(fullPhoneNumber)")
+        //        print("code to remove \(updatedCode)")
+        //        print("clean number is \(replacedNUmber)")
+        
+        return replacedNUmber
+    }
+    
+    fileprivate func splitDataInToSection() {
+        
+        // set section title "" at initial
+        var sectionTitle: String = ""
+        
+        // iterate all records from array
+        for i in 0..<self.matchedUsers.count {
+            
+            // get current record
+            let currentUser = self.matchedUsers[i]
+            
+            // find first character from current record
+            let firstChar = currentUser.firstname.first!
+            
+            // convert first character into string
+            let firstCharString = "\(firstChar)"
+            
+            // if first character not match with past section title then create new section
+            if firstCharString != sectionTitle {
+                
+                // set new title for section
+                sectionTitle = firstCharString
+                
+                // add new section having key as section title and value as empty array of string
+                self.allUsersGrouped[sectionTitle] = []
+                
+                // append title within section title list
+                self.sectionTitleList.append(sectionTitle)
+            }
+            
+            // add record to the section
+            self.allUsersGrouped[firstCharString]?.append(currentUser)
+        }
+        tableView.reloadData()
+    }
+    
+    //MARK: Search controller functions
+    
+    func filteredContentForSearchText(searchText: String, scope: String = "All") {
+        
+        
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        
+    }
+    
+
+    //MARK: UserTableviewCellDelegate
+    
+    func didTapAvatarImage(indexPath: IndexPath) {
+        
+    }
+
+}
